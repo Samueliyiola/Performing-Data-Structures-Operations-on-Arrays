fe66c8924bd9b9ec14314712a0bef194d8d3b4a4
Samueliyiola <105436809+Samueliyiola@users.noreply.github.com>
2023-01-04 01:43:49 +0100

    Add files via upload

---
+++ M.java
@@ -0,0 +1,13 @@
+package ARRAYS;
+
+public class M {
+    public static void main(String[] args) {
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        System.out.println(numbers.indexOf(1000));
+        numbers.print();
+    }
+}
